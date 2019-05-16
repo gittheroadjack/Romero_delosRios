bc696110c9b5069094ce29e67d63cbee709e96a1
gittheroadjack <ale_romero97@hotmail.com>
2019-05-16 16:25:34 +0200

    Añadido controlador de createList

--- src/main/java/evolsoft/concesionario/controller/CocheController.java
+++ src/main/java/evolsoft/concesionario/controller/CocheController.java
@@ -76,5 +76,10 @@ public class CocheController {
 		cocheService.newSell(soldCarDTO.getIdCoche(), soldCarDTO.getIdCliente(),
 				soldCarDTO.getIdVendedor());
 	}
+
+	@PostMapping("/insertList")
+	public void createList(@RequestBody List<CocheDTO> listCocheDto) {
+		cocheService.createList(listCocheDto);
+	}
 	
 }
